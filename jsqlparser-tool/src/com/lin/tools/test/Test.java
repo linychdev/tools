c9d34a2c5da275304c63a3c86aeddfa9a276ef7f
linych <linych@thunisoft.com>
2018-10-11 14:49:58 +0800

    新增测试类

---
+++ jsqlparser-tool/src/com/lin/tools/test/Test.java
@@ -0,0 +1,40 @@
+/*
+ * @(#)Test.java 2018年10月11日下午1:42:01
+ * jsqlparser-tool
+ * Copyright 2018 Thuisoft, Inc. All rights reserved.
+ * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.lin.tools.test;
+
+import com.lin.tools.jsqlparser.WhereUtils;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+
+/**
+ * Test
+ * @author linych
+ * @version 1.0
+ *
+ */
+public class Test {
+    public static void main(String[] args) throws JSQLParserException {
+        String sql = "select * from db_rsyw.t_fyrs_flzw where c_bh_ry in ('123')";
+        Select select = (Select) CCJSqlParserUtil.parse(sql);
+        PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
+        Expression where = plainSelect.getWhere();
+        
+        EqualsTo et = new EqualsTo();
+        et.setLeftExpression(new Column("f_001"));
+        et.setRightExpression(new StringValue("qwe"));
+        where = WhereUtils.addFilter(where, et, "and");
+        System.out.println(where);
+    }
+}
